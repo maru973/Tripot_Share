50853f723ce495f96f660d4757ef19773122e17e
gudemaru <gudemaru@gudemarunoMacBook-Air.local>
2024-04-12 17:42:07 +0900

    カラーの設定、ロゴ追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,38 @@ module.exports = {
   plugins: [
     require('daisyui')
   ],
+  theme: {
+    screens: {
+      'sm': '440px', //ビューでsm:クラスとすると440以上でsm:クラスを適応。元のクラスをスマホ用にしておく
+      'md': '734px', //微妙な横幅のブラウザ、タブレットサイズ以上
+      'lg': '1000px', //pcサイズ以上
+      'xl': '1300px', //pcブラウザでも横幅が広い時
+    },
+    extend: {
+      fontFamily: {
+        main: ['Zen Maru Gothic', 'serif'],
+      },
+    },
+  },
+
+  daisyui: {
+    themes: ["cupcake"],
+  },
   daisyui: {
     themes: [
-      "cupcake"
+      {
+        mytheme: {
+          "base-content": "#021921",
+          "neutral": "#F6F6E9",
+          "primary": "#0CB4F4",
+          "secondary": "#7ED7F9",
+          "accent": "#F68712",
+          "success": "8be17f",
+          "error": "#f46262",
+          "warning": "#f1e747",
+          "info": "#9EE1FB",
+        },
+      },
     ],
   },
 }
