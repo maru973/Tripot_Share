e053e1dbc2759bc501f4f331a2cf9b8f6892410c
honoka <ara4wa12.market@gmail.com>
2024-04-18 11:50:47 +0900

    ルーティング設定、フッターのパス設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "omniauth_callbacks"
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
   root "staticpages#top"
+
+  get '/privacy_policy', to: 'static_pages#privacy_policy'
+  get '/terms_of_service', to: 'static_pages#terms_of_service'
+  get '/contact_us', to: 'static_pages#contact_us'
+
+  resources :lists
+
+  namespace :mypage do
+    resources :posts, only: [:index]
+    resource :settings, only: %i[show update destroy]
+  end
 end
