569e3a4cd411cb4305ca03f1e4e43d21e94ce500
honoka <ara4wa12.market@gmail.com>
2024-04-27 16:53:35 +0900

    geocoder.rbの設定

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,8 +1,3 @@
 class PlansController < ApplicationController
   def new; end
-
-  def spot
-    results = Geocoder.search(params[:name])
-    @latlng =results.first.coordinates
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,9 +24,6 @@ Rails.application.routes.draw do
   resources :courses
   resources :spots, only: %i[create destroy]
 
-  # 指定場所にマーカーをつけるための仮パス
-  get '/spot_entry', to: 'plans#spot', as: 'spot_entry'
-
   namespace :mypage do
     resources :plans, only: [:index]
     resources :courses, only: [:index]
