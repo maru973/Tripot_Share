5fa304b1f7b303899e2807d9e50cf9e87aa57d0a
honoka <ara4wa12.market@gmail.com>
2024-05-02 11:30:31 +0900

    日付の空欄を許可、終了日より開始日が前の日付になるようにバリデーションの追加

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,13 @@
 class Plan < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+
+  validate :start_date_before_end_date, if: -> { start_date.present? && end_date.present? } 
+
+  private
+
+  def start_date_before_end_date
+    if start_date >= end_date
+      errors.add(:start_date, '旅行終了日より前の日付に設定してください。')
+    end
+  end
 end
--- db/migrate/20240501081322_create_plans.rb
+++ db/migrate/20240501081322_create_plans.rb
@@ -2,8 +2,8 @@ class CreatePlans < ActiveRecord::Migration[7.1]
   def change
     create_table :plans do |t|
       t.string :name, null: false
-      t.date :start_date, null: false, default: nil
-      t.date :end_date, null: false, default: nil
+      t.date :start_date
+      t.date :end_date
 
       t.timestamps
     end
