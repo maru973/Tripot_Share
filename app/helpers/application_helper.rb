6f971421bdc130363a963a9dc79192080f283e82
honoka <ara4wa12.market@gmail.com>
2024-05-05 23:04:17 +0900

    フラッシュメッセージのデザイン修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,10 @@
 module ApplicationHelper
-  def flash_background_color(type)
+  def flash_color(type)
     case type.to_sym
     when :info  then "bg-info text-base-content"
     when :notice then "bg-success text-base-content"
     when :warning then "bg-warning text-base-content"
-    when :alert  then "bg-error"
-    when :error  then "bg-error text-base-content"
+    when :alert  then "bg-error text-white"
     else "bg-gray-500"
     end
   end
