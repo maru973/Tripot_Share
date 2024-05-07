7a784363887aa12513ab11b9ca71a756dabb7a56
honoka <ara4wa12.market@gmail.com>
2024-05-07 11:20:30 +0900

    new2ページのパスをnew_spotsに変更

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -6,14 +6,14 @@ class PlansController < ApplicationController
   def create
     @plan = Plan.build(plan_params)
     if @plan.save
-      redirect_to plans_new2_path(@plan), notice: "プランを作成しました"
+      redirect_to new_spots_path(@plan), notice: "プランを作成しました"
     else
       flash.now[:alert] = "プランの作成に失敗しました"
       render :new, status: :unprocessable_entity
     end
   end
 
-  def new2
+  def new_spots
     @plan = Plan.find(params[:id])
     @spot = Spot.new
     @spots = @plan.spots
