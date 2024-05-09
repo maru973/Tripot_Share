2a47e370af7663a5584bdbaac46bca017768c474
honoka <ara4wa12.market@gmail.com>
2024-05-09 17:55:35 +0900

    既存ユーザーにも招待メールを送って招待したplanを自動的に保存できるように設定

--- app/controllers/users/Invitations_controller.rb
+++ app/controllers/users/Invitations_controller.rb
@@ -1,39 +1,52 @@
 class Users::InvitationsController < Devise::InvitationsController
   def create
-      super
-      @member = Member.new
-      #Memberテーブルに招待したいユーザーのidを入れる。
-      @member.user_id = User.find_by(email: params[:user][:email]).id
-      @member.plan_id = params[:user][:plan_id]
-      @member.save
+    @user =User.new
+    user_email = params[:user][:email]
+    plan_id = params[:user][:plan_id]
+    if User.find_by(email: user_email.downcase).present? #　既存ユーザーの処理
+      user_id = User.find_by(email: user_email.downcase).id
+      user = User.find(user_id)
+      if user.plan_ids.include?(plan_id.to_i)
+        redirect_to root_path, alert: "このリストは登録済みです。"
+      else
+        user.invite!(current_user)
+        #Memberテーブルに招待したいユーザーのidとリストのidを入れる。
+        user.invited_plan_id = plan_id
+        user.save
+        redirect_to root_path, notice: t('devise.invitations.send_instructions', email: user_email)
+      end
+    elsif User.invite!(email: user_email, invited_plan_id: plan_id).valid? # 新規ユーザーの処理
+      redirect_to plans_path(current_user), notice: t('devise.invitations.send_instructions', email: user_email)
+    else
+      flash[:notice] = t('devise.invitations.failure')
+      render 'new', locals: { plan: plan_id }
+    end
   end
 
-  # def create
-  #   user_email = params[:user][:email]
-  #   plan_id = params[:user][:plan_id]
-  #   user_id = User.find_by(email: user_email.downcase).id
-  #   if User.find_by(email: user_email.downcase).present? #　既存ユーザーの処理
-  #     user = User.find(user_id)
-  #     if user.plan_ids.include?(plan_id.to_i)
-  #       redirect_to root_path, alert: "このリストは登録済みです。"
-  #     else
-  #     user.invite!
-  #     @member = Member.new
-  #     #Memberテーブルに招待したいユーザーのidとリストのidを入れる。
-  #     @member.user_id = user_id
-  #     @member.plan_id = plan_id
-  #     @member.save
-  #     redirect_to root_path, notice: t('.send_instructions')
-  #     end
-  #   elsif User.invite!(email: user_email, plan_id: plan_id) # 新規ユーザーの処理
-  #     redirect_to plans_path(current_user), notice: t('.send_instructions')
-  #     @member = Member.new
-  #     @member.user_id = user_id
-  #     @member.plan_id = plan_id
-  #     @member.save
-  #   else
-  #     flash[:notice] = t('.failure')
-  #     render 'new', locals: { plan: plan_id }
-  #   end
-  # end
+  def update
+    raw_invitation_token = update_resource_params[:invitation_token]
+    self.resource = accept_resource
+    invitation_accepted = resource.errors.empty?
+
+    user_id = resource.id
+    user = User.find(user_id)
+    Member.create(user_id: user_id, plan_id: user.invited_plan_id)
+    yield resource if block_given?
+
+    if invitation_accepted
+      if resource.class.allow_insecure_sign_in_after_accept
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message :notice, flash_message if is_flashing_format?
+        resource.after_database_authentication
+        sign_in(resource_name, resource)
+        respond_with resource, location: plans_path
+      else
+        set_flash_message :notice, :updated_not_active if is_flashing_format?
+        respond_with resource, location: new_session_path(resource_name)
+      end
+    else
+      resource.invitation_token = raw_invitation_token
+      respond_with_navigational(resource) { render :edit, status: :unprocessable_entity }
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_09_035154) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_09_080625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_09_035154) do
     t.string "provider"
     t.string "uid"
     t.string "name", default: "", null: false
+    t.integer "invited_plan_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
