f1c4a07670f4a3101a693184ed13cebef330f2c9
honoka <ara4wa12.market@gmail.com>
2024-05-10 14:23:53 +0900

    turbo_frameを使って招待ページをモーダルで表示

--- app/controllers/users/Invitations_controller.rb
+++ app/controllers/users/Invitations_controller.rb
@@ -1,9 +1,8 @@
 class Users::InvitationsController < Devise::InvitationsController
   def create
-    @name = current_user.name
     @user =User.new
-    user_email = params[:user][:email]
-    plan_id = params[:user][:plan_id]
+    user_email = params[:email]
+    plan_id = params[:plan_id]
 
      # 既存ユーザーの処理
     if User.find_by(email: user_email.downcase).present?
