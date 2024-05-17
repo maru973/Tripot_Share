3bc097e8309a765b66b1aa6a5b288d8d965e731f
honoka <ara4wa12.market@gmail.com>
2024-05-17 16:14:51 +0900

    本番環境でメールを送信できるように設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,16 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.action_mailer.default_url_options = { protocol: 'https', host: 'tripot-share.fly.dev' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com',
+   port:                  587,
+   domain:               'tripot-share.fly.dev',
+   user_name:            ENV['GMAIL_USERNAME'],
+   password:             ENV['GMAIL_PASSWORD'],
+   authentication:       'plain',
+   enable_starttls_auto:  true
+  }
+
 end
