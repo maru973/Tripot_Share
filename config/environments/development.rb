993925cd9a87cdc0111eb18f13e29c0677ca22e5
honoka <ara4wa12.market@gmail.com>
2024-05-22 12:44:57 +0900

    開発環境でのメールの設定を修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,15 +78,4 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-   address:              'smtp.gmail.com',
-   port:                  587,
-   domain:               'tripot-share.fly.dev',
-   user_name:            ENV['GMAIL_USERNAME'],
-   password:             ENV['GMAIL_PASSWORD'],
-   authentication:       'plain',
-   enable_starttls_auto:  true
-  }
 end
