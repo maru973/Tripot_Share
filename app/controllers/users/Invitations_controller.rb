66ab264b5039fa51c37892de1b57984189a1b1f7
honoka <ara4wa12.market@gmail.com>
2024-05-23 13:10:58 +0900

    招待メールを送信後、フラッシュメッセージを表示

--- app/controllers/users/Invitations_controller.rb
+++ app/controllers/users/Invitations_controller.rb
@@ -4,26 +4,37 @@ class Users::InvitationsController < Devise::InvitationsController
     user_email = params[:user][:email]
     plan_id = params[:user][:plan_id]
 
-    # 既存ユーザーの処理
-    if User.find_by(email: user_email.downcase).present?
-      user_id = User.find_by(email: user_email.downcase).id
-      user = User.find(user_id)
-
-      # すでに招待される側がプランを持っているか確認
-      if user.plan_ids.include?(plan_id.to_i)
-        redirect_to plan_path(plan_id), alert: t('devise.invitations.already_registered_plan', email: user_email)
+    respond_to do |format|
+      # 既存ユーザーの処理
+      if User.find_by(email: user_email.downcase).present?
+        user_id = User.find_by(email: user_email.downcase).id
+        user = User.find(user_id)
+      
+        # すでに招待される側がプランを持っているか確認
+        if user.plan_ids.include?(plan_id.to_i)
+          format.turbo_stream do
+            flash.now[:alert] = t('devise.invitations.already_registered_plan', email: user_email)
+            render turbo_stream: turbo_stream.prepend("flash", partial: "shared/flash_message")
+          end
+        else
+          user.invite!(current_user)
+          # invited_plan_idに一時的にplan_idを保存
+          user.invited_plan_id = plan_id
+          user.save
+          format.turbo_stream do
+            flash.now[:notice] = t('devise.invitations.send_instructions', email: user_email)
+            render turbo_stream: turbo_stream.prepend("flash", partial: "shared/flash_message")
+          end
+        end
+      elsif User.invite!(email: user_email, invited_plan_id: plan_id, name: "仮ユーザー名").valid? # 新規ユーザーの処理
+        format.turbo_stream do
+          flash.now[:notice] = t('devise.invitations.send_instructions', email: user_email)
+          render turbo_stream: turbo_stream.prepend("flash", partial: "shared/flash_message")
+        end
       else
-        user.invite!(current_user)
-        # invited_plan_idに一時的にplan_idを保存
-        user.invited_plan_id = plan_id
-        user.save
-        redirect_to plan_path(plan_id), notice: t('devise.invitations.send_instructions', email: user_email)
+        flash[:alert] = t('devise.invitations.failure')
+        render 'new', locals: { id: plan_id }
       end
-    elsif User.invite!(email: user_email, invited_plan_id: plan_id, name: "仮ユーザー名").valid? # 新規ユーザーの処理
-      redirect_to plan_path(plan_id), notice: t('devise.invitations.send_instructions', email: user_email)
-    else
-      flash[:notice] = t('devise.invitations.failure')
-      render 'new', locals: { id: plan_id }
     end
   end
 
