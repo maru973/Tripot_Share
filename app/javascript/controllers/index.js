6dc132791c8d2dd8f8f1de1398307882ca3966bd
honoka <ara4wa12.market@gmail.com>
2024-05-24 22:30:47 +0900

    コピーボタンを押したらURLがコピーされる

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import ClipboardController from "./clipboard_controller"
+application.register("clipboard", ClipboardController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
