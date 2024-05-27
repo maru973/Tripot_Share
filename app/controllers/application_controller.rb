297e2af3938c1376cd1033fd930277408e4ad0eb
honoka <ara4wa12.market@gmail.com>
2024-05-27 16:32:42 +0900

    プロフィールの編集ができないように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def check_guest
-    email = resource&.email || params[:user][:email].downcase
+    email = current_user&.email || resource&.email || params[:user][:email].downcase
     if email === 'guest@example.com'
       redirect_to root_path, alert: 'ゲストユーザーの更新・削除はできません'
     end
