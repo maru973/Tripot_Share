6c79df7733e78b0b72a4d70d8d43e4c134d935b4
honoka <ara4wa12.market@gmail.com>
2024-05-28 14:18:18 +0900

    ランキングテーブルの作成

---
+++ db/migrate/20240528051555_create_spot_rankings.rb
@@ -0,0 +1,11 @@
+class CreateSpotRankings < ActiveRecord::Migration[7.1]
+  def change
+    create_table :spot_rankings do |t|
+      t.integer :point, null: false, default: 0
+      t.references :user, null: false, foreign_key: true
+      t.references :planned_spot, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
