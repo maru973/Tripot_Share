d9275a061aa9bd98b17ca22b169d46dc98f8de74
honoka <ara4wa12.market@gmail.com>
2024-06-04 10:18:26 +0900

    コーステーブルからplanned_spot_idのカラムを削除

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,5 +6,21 @@ class CoursesController < ApplicationController
 
   def create
     @plan = Plan.find(params[:plan_id])
+    @course = @plan.courses.build(course_params)
+    @location = Spot.find_or_initialize_by(name: @plan.location)
+      if @location.new_record?
+        results = Geocoder.search(@plan.location)
+        @latlng = results.first.coordinates
+        @location.latitude = @latlng[0]
+        @location.longitude = @latlng[1]
+        @location.address = results.first.address
+        @location.save
+      end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:start_location, :end_location)
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,5 @@
 class Course < ApplicationRecord
   belongs_to :plan
-  belongs_to :planned_spots
 
   validates :start_location, presence: true
   validates :end_location, presence: true
