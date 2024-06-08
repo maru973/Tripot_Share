77e013179eb756c5753a8b9f252c6a67d93d65ca
honoka <ara4wa12.market@gmail.com>
2024-06-08 17:56:30 +0900

    fetch APIがなくてもパラメーターでコントローラーに渡せていたため該当コードを削除

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -21,31 +21,6 @@ export default class extends Controller {
     const body = { 
       row_order_position: evt.newIndex,
       spot_id: evt.item.dataset.spotId
-     }
-    fetch(evt.item.dataset.sortableUrl, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-        'Accept': 'text/vnd.turbo-stream.html'  // Turbo Streamのレスポンスを受け入れる
-      },
-      body: JSON.stringify(body)
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.text();  // Turbo StreamはHTMLとして返される
-    })
-    .then(turboStream => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(turboStream, 'text/html');
-      doc.body.querySelectorAll('turbo-stream').forEach(stream => {
-        document.documentElement.appendChild(stream);
-      });
-    })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
-    });
-  }
+    }
+  };
 }
