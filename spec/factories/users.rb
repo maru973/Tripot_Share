33d9448225f2db9ca3ac30b0f4d5fa48cbc3bd66
honoka <ara4wa12.market@gmail.com>
2024-07-01 21:32:18 +0900

    ユーザーモデルスペックにテストコードを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { 'user' }
-    email { 'example.example.com' }
+    email { 'example@example.com' }
     password { '12345678' }
     password_confirmation { '12345678' }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it '名前、メールアドレスがあり、パスワードは6文字以上であれば有効であること' do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
+  it 'メールはユニークであること' do
+    user1 = create(:user)
+    user2 = build(:user)
+    user2.email = user1.email
+    user2.valid?
+    expect(user2.errors[:email]).to include('はすでに存在します')
+  end
 
 end
