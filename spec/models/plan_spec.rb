cdcb8662b58ee5ea4bdddd963ccec115cdc20498
honoka <ara4wa12.market@gmail.com>
2024-07-02 18:06:13 +0900

    プランモデルスペックのテストコード追加

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Plan, type: :model do
-  context ''
+  it '全フィールドが入力されている場合有効になること' do
+    plan = build(:plan)
+    expect(plan).to be_valid
+  end
+
+  it 'プラン名が空欄の場合は無効になること' do
+    plan = build(:plan)
+    plan.name = nil
+    plan.valid?
+    expect(plan.errors[:name]).to include('を入力してください')
+  end
+
+  it '行き先が空欄の場合は無効になること' do
+    plan = build(:plan)
+    plan.location = nil
+    plan.valid?
+    expect(plan.errors[:location]).to include('を入力してください')
+  end
 end
