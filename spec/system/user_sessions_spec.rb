1ab60055991338f8eff866473f4c7b848006b12d
honoka <ara4wa12.market@gmail.com>
2024-07-10 10:15:52 +0900

    ログインができない場合のテストコード追加

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "UserSessions", type: :system do
   it 'ログインができること' do
     visit '/users/sign_in'
     fill_in 'メールアドレス', with: user.email
-    fill_in 'パスワード', with: '12345678'
+    fill_in 'パスワード', with: user.password
     click_button 'ログイン'
     Capybara.assert_current_path("/plans", ignore_query: true)
     expect(page).to have_content('ログインしました'), 'フラッシュメッセージ「ログインしました」が表示されていません'
@@ -18,13 +18,23 @@ RSpec.describe "UserSessions", type: :system do
 
   context 'ログインができない' do
     it 'パスワードが間違っている場合' do
-
+      visit '/users/sign_in'
+      fill_in 'メールアドレス', with: user.email
+      fill_in 'パスワード', with: '1234567'
+      click_button 'ログイン'
+      expect(page).to have_content('メールアドレスまたはパスワードが違います'), 'フラッシュメッセージ「メールアドレスまたはパスワードが違います」が表示されていません'
     end
 
     it 'メールアドレスが間違っている場合' do
+      visit '/users/sign_in'
+      fill_in 'メールアドレス', with: 'sample@example.com'
+      fill_in 'パスワード', with: user.password
+      click_button 'ログイン'
+      expect(page).to have_content('メールアドレスまたはパスワードが違います'), 'フラッシュメッセージ「メールアドレスまたはパスワードが違います」が表示されていません'
     end
   end
 
   it 'ログアウトができること' do
+    
   end
 end
