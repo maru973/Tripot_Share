b7bdf2a0bba8538d99cb8a52a3f843767dcf8678
honoka <ara4wa12.market@gmail.com>
2024-07-11 21:55:47 +0900

    条件分追加

--- spec/system/plans_spec.rb
+++ spec/system/plans_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe "Plans", type: :system do
     driven_by(:rack_test)
   end
 
+  describe 'プラン作成' do
+    context 'ログイン済み' do
+      it 'プランが作成されスポット登録ページに遷移すること' do
+      end
+
+      it 'プラン作成ができないこと' do
+      end
+    end
+
+    context '未ログイン' do
+      it 'ログインページにリダイレクトされること' do
+      end
+    end
+  end
 end
