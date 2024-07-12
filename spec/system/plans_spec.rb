40f37f6fdfc74877dbc9ae242cca0cdc7a2bd54c
honoka <ara4wa12.market@gmail.com>
2024-07-12 23:23:37 +0900

    プラン作成成功のテストコードを追加

--- spec/system/plans_spec.rb
+++ spec/system/plans_spec.rb
@@ -5,9 +5,26 @@ RSpec.describe "Plans", type: :system do
     driven_by(:rack_test)
   end
 
+  let(:user) { create(:user) }
+
   describe 'プラン作成' do
     context 'ログイン済み' do
+      before do
+        login_as(user)
+        visit '/plans'
+        click_link '旅行プランをつくる'
+      end
+
       it 'プランが作成されスポット登録ページに遷移すること' do
+        expect { 
+          fill_in 'プラン名', with: 'プラン１'
+          find("option[value='京都府']").select_option
+          click_button '作成'
+        }.to change { Plan.count }.by(1)
+        plan = Plan.last
+        Capybara.assert_current_path("/plans/#{plan.id}/new_spots", ignore_query: true)
+        expect(page).to have_content("#{plan.name}を作成しました"), 'フラッシュメッセージ「#{プラン名}を作成しました」が表示されていません'
+        expect(page).to have_content('スポット登録'), '「スポット登録」の文言が表示されていません'
       end
 
       it 'プラン作成ができないこと' do
